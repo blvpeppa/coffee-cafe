6c4e3154a1e85863953abf620d4de36b30958642
blvpeppa <blvpeppgram@gmail>
2025-04-13 17:35:50 +0200

    git commit -m

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BgImage from "../../assets/image_1.jpg";
+import BgImage from "../../assets/bg_4.jpg";
 import CEOImage from "../../assets/CEO.jpg";
 import About1 from "../../assets/about-1.jpg";
 import Banner from "../Banner/Banner";
@@ -7,45 +7,19 @@ const About = () => {
   return (
     <>
       <div
-            className="relative w-full bg-cover bg-no-repeat bg-center min-h-[500px] sm:min-h-[600px] md:min-h-[300px] lg:h-screen flex items-center justify-center"
+            className="relative w-full bg-cover bg-no-repeat bg-center min-h-[300px] sm:min-h-[100px] md:min-h-[100px] lg:h-screen flex items-center justify-center"
             style={{ backgroundImage: `url(${BgImage})` }}
           >
             {/* Overlay */}
-            <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
+            <div className="absolute inset-0 bg-gradient-to-r from-green-700 bg-opacity-50 z-10"></div>
       
             {/* Hero Content */}
             <section className="relative z-20 w-full">
               <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 text-center sm:text-left">
                 <div className="max-w-xl mx-auto sm:mx-0 py-16 sm:py-24 lg:py-32">
                   <h1 className="text-3xl sm:text-5xl font-bold text-white leading-tight">
-                    RABBIT <span className="text-green-600">GENETIC</span> CENTER
+                    KNOW MORE ABOUT <span className="text-green-500">KRC</span>
                   </h1>
-                  <p className="mt-4 text-base sm:text-lg text-gray-300">
-                    Leading innovation in sustainable rabbit farming & genetics.
-                  </p>
-      
-                  {/* CTA Button */}
-                  <div className="mt-6 flex justify-center sm:justify-start">
-                    <a
-                      href="/#services"
-                      className="inline-flex items-center gap-2 border border-white px-6 py-2 sm:px-8 sm:py-3 rounded-sm text-sm sm:text-base text-white hover:bg-white hover:text-green-700 transition duration-300"
-                    >
-                      Learn More
-                      <svg
-                        className="w-5 h-5"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M17 8l4 4m0 0l-4 4m4-4H3"
-                        />
-                      </svg>
-                    </a>
-                  </div>
                 </div>
               </div>
             </section>
@@ -104,10 +78,8 @@ const About = () => {
           </p>
         </div>
       </section>
-
       <Banner />
-
-    </>
+      </>
   );
 };
 
