78506f983403d42d66d26ef4a2709254ffcf7316
kigalirabbitcenter <kigalirabbit@gmail.com>
2025-04-30 14:11:21 +0300

    git commit -m

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -138,7 +138,7 @@ const About = () => {
               <p className="mt-2 text-gray-700">Outgrowers</p>
             </div>
             <div className="bg-gray-100 p-6 rounded shadow-md">
-              <h2 className="text-4xl font-bold text-green-600">15</h2>
+              <h2 className="text-4xl font-bold text-green-600">16</h2>
               <p className="mt-2 text-gray-700">Veterinarians</p>
             </div>
             <div className="bg-gray-100 p-6 rounded shadow-md">
